a2553c3c2f2fee24424a939a96fb862da8351dde
sundaybest3 <163014658+sundaybest3@users.noreply.github.com>
2024-12-29 02:33:23 +0900

    Update NounSmart_irregulars.py

--- pages/NounSmart_irregulars.py
+++ pages/NounSmart_irregulars.py
@@ -16,13 +16,15 @@ qr = qrcode.QRCode(version=1, box_size=10, border=5)
 qr.add_data(qr_data)
 qr.make(fit=True)
 
+# Add QR code to Streamlit
+st.image(buffer, caption="Scan this QR code to NounSmart_irregulars.", use_container_width=True) 
+
 # Convert QR code to an image
 qr_image = qr.make_image(fill="black", back_color="white")
 buffer = BytesIO()
 qr_image.save(buffer, format="PNG")
 buffer.seek(0)
 
-# Add QR code to Streamlit
-st.image(buffer, caption="Scan this QR code to NounSmart_irregulars.", use_container_width=True)
+
 
 
