b3d943c322e9d59d5a91bafe513fb56be8247882
sundaybest3 <163014658+sundaybest3@users.noreply.github.com>
2024-12-29 03:17:55 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("About sundaybest")
-st.write("Pure beginner of streamlit")
+st.title("About Sundaybest")
+st.write("## ☘️ Pure beginner of streamlit and coding.")
+st.write("## ☘️ High school English teacher in South Korea.")
