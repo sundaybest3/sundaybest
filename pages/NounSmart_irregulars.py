9bab8a954566f48f1c1dd0137a4c7dd3a776f53f
sundaybest3 <163014658+sundaybest3@users.noreply.github.com>
2024-12-29 02:18:00 +0900

    Update NounSmart_irregulars.py

--- pages/NounSmart_irregulars.py
+++ pages/NounSmart_irregulars.py
@@ -19,5 +19,5 @@ buffer = BytesIO()
 qr_image.save(buffer, format="PNG")
 buffer.seek(0)
 
-# Add QR code to Streamlit
-st.image(buffer, caption="Scan this QR code to visit our page!", use_column_width=True)
+st.image("path/to/qr_code.png", caption="Scan this QR code to visit our page!", use_container_width=True)
+
