4c2cdac0e9360e42822c4b7df0675d92686619b8
sundaybest3 <163014658+sundaybest3@users.noreply.github.com>
2024-12-29 03:11:56 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title("Welcome to Sundaybest's Home!")
-         
-image_url = "https://github.com/sundaybest3/sundaybest/blob/main/home_image.png"
+
+# Raw URL 사용
+image_url = "https://raw.githubusercontent.com/sundaybest3/sundaybest/main/home_image.png"
 
 st.image(image_url, caption="Home Image", use_container_width=True)
