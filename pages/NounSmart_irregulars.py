95405193d90571c91d98643463aade1f9b67499f
sundaybest3 <163014658+sundaybest3@users.noreply.github.com>
2024-12-29 02:34:47 +0900

    Update NounSmart_irregulars.py

--- pages/NounSmart_irregulars.py
+++ pages/NounSmart_irregulars.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 st.title("NounSmart: Irregulars")
 st.write("This app is for practicing spelling of irregular plural nouns.")
+st.write("Click the link or scan the QR code to visit the app.")
 st.write("Choose a level and get all the answers correct.")
 st.write("Good luck! 🍀")
-st.markdown("[Click here to visit NounSmart: Irregulars.](https://nounsmartirregulars-5xvnwrz796lsebgkjmyotk.streamlit.app/)")
+st.markdown("[Link: Irregulars.](https://nounsmartirregulars-5xvnwrz796lsebgkjmyotk.streamlit.app/)")
 
 
 import qrcode
@@ -16,8 +17,6 @@ qr = qrcode.QRCode(version=1, box_size=10, border=5)
 qr.add_data(qr_data)
 qr.make(fit=True)
 
-# Add QR code to Streamlit
-st.image(buffer, caption="Scan this QR code to NounSmart_irregulars.", use_container_width=True) 
 
 # Convert QR code to an image
 qr_image = qr.make_image(fill="black", back_color="white")
@@ -25,6 +24,8 @@ buffer = BytesIO()
 qr_image.save(buffer, format="PNG")
 buffer.seek(0)
 
+# Add QR code to Streamlit
+st.image(buffer, caption="Scan this QR code to NounSmart_irregulars.", use_container_width=True) 
 
 
 
